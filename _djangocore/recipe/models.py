1eafa1f1e7fdf449acd239f02f12a4545a069186
Michael Paulauski <mike10010100@gmail.com>
2013-09-10 13:09:29 -0400

    Added basic database entries for recipes

--- _djangocore/recipe/models.py
+++ _djangocore/recipe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    create_date = models.DateTimeField('date published')
+    description = models.CharField(max_length=5000)
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe)
+    recipe_text = models.CharField(max_length=5000)
+    step_num = models.IntegerField(default=0)
