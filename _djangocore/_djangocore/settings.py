5bd1eb73420b4f981c5413e1ff54fd40ddcb79da
Michael Paulauski <mike10010100@gmail.com>
2013-09-10 23:17:01 -0400

    updated models.py and...probably some other stuff too. Maybe not. We'll see

--- _djangocore/_djangocore/settings.py
+++ _djangocore/_djangocore/settings.py
@@ -6,6 +6,8 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
+    ('Michael Paulauski', "mike10010100@gmail.com"),
+    ('Chris Kelley', 'ryunoneiru@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -122,9 +124,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'recipe',
 )
 
--- _djangocore/recipe/models.py
+++ _djangocore/recipe/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
+    #user = models.user()
     create_date = models.DateTimeField('date published')
     description = models.CharField(max_length=5000)
+
+class Tag(models.Model):
+	title = models.CharField(max_length=64)
+	#tagtype = 
 
 class Ingredient(models.Model):
     recipe = models.ForeignKey(Recipe)
