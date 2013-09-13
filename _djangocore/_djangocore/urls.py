60cb54b4cc490b4de54c9ae28809139421bc98d8
Michael Paulauski <mike10010100@gmail.com>
2013-09-13 15:08:15 -0400

    Got it to hit home at just the root site address. Now to get the index.html set up

--- _djangocore/_djangocore/urls.py
+++ _djangocore/_djangocore/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^recipe/', include('recipe.urls')),
+    # url(r'^recipe/', include('recipe.urls')),
+    url(r'^$', include('recipe.urls')),
 )
