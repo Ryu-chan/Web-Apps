d1bfcf7d7b3312b7c65e107e92c67ab8af1d5102
Michael Paulauski <mike10010100@gmail.com>
2013-09-17 15:02:33 -0400

    Booyah! Got the homepage up and running. Now to get the sign up and sign in working.

--- _djangocore/recipe/views.py
+++ _djangocore/recipe/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the recipe index.")
+    return render(request, 'recipe/index.html')
